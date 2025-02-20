225e4ba7af5b375d2389271e309999234843b8fd
unknown <chitkoko124706@gmail.com>
2025-02-20 09:46:26 +0630

    adbanner

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,18 @@ export default function RootLayout({
           src="//pl25920468.effectiveratecpm.com/5b/bb/82/5bbb82961dbc52da117babc3dd9c7a3c.js"
           async
         ></script>
+        <script
+          async
+          data-cfasync="false"
+          src="//pl25920512.effectiveratecpm.com/841fc6cde6e92c4531f3519d15ff485b/invoke.js"
+        ></script>
       </head>
       <body className={inter.className} suppressHydrationWarning>
         <SupabaseProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
+            <div id="container-841fc6cde6e92c4531f3519d15ff485b"></div>
+
             <main className="flex-grow bg-background">{children}</main>
             <Footer />
           </div>
